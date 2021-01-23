f5abf610decedfcee01a8ed5e5efa6bb66926274
Kim Hyun-Jun <hyunjun5216@naver.com>
2021-01-23 23:56:58 +0900

    gender

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const sayHi = (Human) => {
-  console.log(`Hello ${Human.name}, age: ${Human.age}`)
+  console.log(`Hello ${Human.name}, age: ${Human.age}, gender: ${Human.gender}`)
 }
 
 const Human = {
   name: 'Hyunjun',
-  age: 17
+  age: 17,
+  gender: 'male'
 }
 
 sayHi(Human)
